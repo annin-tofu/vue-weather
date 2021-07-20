208aedccd2c01d551057fc0b3fbf75ab4b229d79
annin-tofu <yuok5770@gmail.com>
2021-07-21 00:37:47 +0900

    styling

--- src/App.vue
+++ src/App.vue
@@ -66,7 +66,31 @@ main {
 
   background-image: linear-gradient(
     to bottom,
-    rgba(0, 0, 0, 0.25) rgba(0, 0, 0, 0.75)
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
   );
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+
+  color: #313131;
+  font-size: 20px;
+
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
 }
 </style>
